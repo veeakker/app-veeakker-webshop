b508ebe60722da7354307792d046f11217c844fe
Siebe Dreesen <siebedreesen@icloud.com>
2021-09-15 14:26:19 +0200

    Account with address

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -218,8 +218,8 @@
   :class (s-prefix "foaf:Person")
   :properties `((:first-name :string ,(s-prefix "foaf:firstName"))
                 (:last-name :string ,(s-prefix "foaf:familyName")))
-  ;; :has-one `((postal-address :via ,(s-prefix "schema:PostalAddress")
-  ;;                            :as "postal-address"))
+  :has-one `((postal-address :via ,(s-prefix "schema:PostalAddress")
+                             :as "postal-address"))
   :has-many `((account :via ,(s-prefix "foaf:account")
                        :as "accounts"))
   :resource-base (s-url "http://veeakker.be/people/")
@@ -234,7 +234,6 @@
 
   :resource-base (s-url "http://mu.semte.ch/vocabularies/accounts/")
   :on-path "accounts")
-
 
 ;; We would like to start using the following
 ;;
