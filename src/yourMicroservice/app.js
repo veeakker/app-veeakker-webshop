837e23b7fd8e44af20c497250a966b619c5b5ec3
AbdelmajidAP <aabdelmajid005@gmail.com>
2023-01-20 12:02:31 +0100

    microService aangepast

--- src/yourMicroservice/app.js
+++ src/yourMicroservice/app.js
@@ -1,9 +1,6 @@
 import { app, query } from 'mu';
 
-app.get('/', function( req, res ) {
-  res.send('bbbbbbbbbbbbbbbb');} );
-
-app.get('/export', async function( req, res ) {
+app.get('/', async function( req, res ) {
   let results = await query (
     `
 SELECT DISTINCT * WHERE { GRAPH <http://mu.semte.ch/graphs/public> { ?s a <http://veeakker.be/vocabularies/shop/Basket>. ?s <http://veeakker.be/vocabularies/shop/orderLine> ?orderLine. ?orderLine <http://veeakker.be/vocabularies/shop/hasOffering> ?ofering. ?ofering <http://purl.org/goodrelations/v1#hasPriceSpecification> ?priceSpec. ?priceSpec <http://purl.org/goodrelations/v1#hasCurrencyValue> ?price. 
