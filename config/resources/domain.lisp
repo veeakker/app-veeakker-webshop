511c26ddf5814c671d03379b104835016f5e34c6
Aad Versteden <madnificent@gmail.com>
2023-01-23 10:29:27 +0100

    Add delivery-place to basket

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -176,6 +176,8 @@
                 (:status-changed-at :datetime ,(s-prefix "veeakker:statusChangedAt")))
   :has-many `((order-line :via ,(s-prefix "veeakker:orderLine")
                           :as "order-lines"))
+  :has-one `((delivery-place :via ,(s-prefix "veeakker:deliveryAt")
+                             :as "delivery-place"))
   :resource-base (s-url "http://veeakker.be/baskets/")
   :on-path "baskets")
 
